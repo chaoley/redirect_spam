70589968c0c57c0ae50ebaaa868d934ed45a0b61
chaoley <chaoley@users.noreply.github.com>
2014-08-06 17:03:01 +0930

    Added Spam Strings

---
+++ spam_strings.php
@@ -0,0 +1,5 @@
+<?php
+
+$spamStrings = array('search engine', 'increase ranking', 'casino', 'internet marketing', 'online marketing', 'search results', 'search rankings', 'grow your business', 'sales goals', 'page of Google', 'link building');
+
+?>
